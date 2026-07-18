e90f6a92bb02f95e66a6af6c27dbb689cf108497
Muhammad Zaeem Sajjad <lethalzaeem@gmail.com>
2026-07-18 23:17:00 +0500

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/react-plugin' // or your current react plugin
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // 👈 Only needed for GitHub Pages. Match your repo name exactly.
+  base: '/Football-Scoring-App/',
 })
